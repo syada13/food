3b9ece1f4d88a026ede0cfb58f12b212409f7d72
syadav <syadav@gozego.com>
2022-07-06 13:08:21 +0530

    Creating new restaurant app using react native

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,18 @@
+import {React} from 'react';
+import {Text,View,StyleSheet} from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <SearchBar/>
+            <Text>Search Screen</Text>
+        </View>
+    );
+
+};
+
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
