3b8500be56a3848de66741b93892b947da8aef74
syadav <syadav@gozego.com>
2022-07-07 11:46:55 +0530

    Added logic for asyncronus api call to pull restaurants serving searched food

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,29 @@
 import React, {useState} from 'react';
 import {Text,View,StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
 
     const [term,setTerm] = useState(" ");
+    const [restaurants,setRestaurants] = useState([]);
+
+    const searchApi= async() => {
+        const response = await yelp.get('/search',{
+            params:{
+                term,
+                limit:50,
+                location: 'eden prarie'
+            }
+        });
+       setRestaurants(response.data.businesses);
+    };
+
     return (
         <View>
-            <SearchBar 
-            term={term} 
-            onTermChange={newTerm => setTerm(newTerm)}
-            onTermSubmit={() => console.log('term was submitted')}/>
+            <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi}/>
             <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            <Text>We have found: {restaurants.length} restaurants</Text>
         </View>
     );
 
