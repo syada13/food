223fae6a3d3ea2d9ba3fc9a72779c424a5f386a1
syadav <syadav@gozego.com>
2022-07-09 11:21:30 +0530

    Using flatlist to display the scrollable restaurants list

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import {View,Text,StyleSheet,FlatList} from 'react-native';
+
 
 
 const RestaurantsList = ({title,restaurants}) => {
@@ -7,7 +8,18 @@ const RestaurantsList = ({title,restaurants}) => {
     return (
         <View>
             <Text style={styles.titleStyle}>{title}</Text>
-            <Text>{restaurants.length}</Text>
+            <FlatList
+            horizontal={false}
+            data={restaurants}
+            keyExtractor={restaurant => restaurant.id}
+            renderItem={({item}) => {
+                return <Text>{item.name}</Text>
+
+            }}
+            />
+
+         
+            
         </View>
 
     );
